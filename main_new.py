dca85f5e77be108a3804e717145afe426d0bfc3c
Ba-So <Bastian.Sommerfeld@gmail.com>
2018-11-29 10:09:38 +0100

    added IOhandler

--- main_new.py
+++ main_new.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 import numpy as np
-import xarray as xr
-from modules.paralleldecorators import Mp, ParallelNpArray, shared_np_array
-from modules.debugdecorators import TimeThis, PrintArgs, PrintReturn
-from modules.cio import glob_files
-from modules.
+import sys as sys
+from decorators.paralleldecorators import Mp, ParallelNpArray, shared_np_array
+from decorators.debugdecorators import TimeThis, PrintArgs, PrintReturn
+import modules.cio as cio
+
 
 mp = Mp(False, 2)
 class Operations(object):
@@ -16,14 +16,10 @@ class Operations(object):
 
 class CoarseGrain(Operations):
 
-    def __init__(self, path, experiment_name):
+    def __init__(self, path, grid, data):
         self._ready = True
-        self._path = path + experiment_name + '/'
-        self.data = {}
-        self.grid_nfo = {}
-        self._experiment_name = experiment_name
-        self.find_files(self._path, self._experiment_name)
-        self.load_necessary(self._datafile, self._gridfile)
+        self.IO = cio.IOcontroller(path, grid, data)
+        self.prepare()
 
     def set_mp(self, switch, num_procs):
         Mp.toggle_switch(switch)
@@ -32,56 +28,29 @@ class CoarseGrain(Operations):
     def set_ready(self, state):
         self._ready = state
 
-    def find_files(self, path, ex_name):
-        #look for data output
-        look_for = ex_name + "*_slice.nc"
-        files = glob_files(path, look_for)
-        setattr(self, '_datafile', files[0])
-        look_for = "icon*-grid_refined_*.nc"
-        files = glob_files(path, look_for)
-        setattr(self, '_gridfile', files[0])
+    def prepare(self):
+        xlat = self.IO.load_from('grid', 'lat')
+        xlon = self.IO.load_from('grid', 'lon')
+        cell_area = self.IO.load_from('grid', 'cell_area')
+        self.gridnfo = [[i, cell_area[i], [xlon[i], xlat[i]]] for i in range(len(xlat))]
 
-    def load_necessary(self, datafile, gridfile):
-        '''to guarante that all the basic necesities are met'''
-        #extract necessary variables from datafile
-        nec_data = [
-            'RHO',
-            'vlat',
-            'vlon',
-            ]
-        path = datafile
-        self.load_data(path, nec_data)
-        #extract necessary variables from gridfile
-        nec_grid = [
-            'coords',
-            'member_idx',
-            'member_rad',
-            'area_member_idx',
-            'cell_area',
-            'coarse_area',
-        ]
-        path = gridfile
-        self.load_data(path, nec_grid)
-
-    def load_data(self, path, list):
-        with xr.open_dataset(path) as ds:
-            for variable in list:
-                try:
-                    self.data.update({variable : shared_np_array(np.shape(ds[variable].values))})
-                    self.data[variable][:,] = ds[variable].values[:,]
-                except KeyError:
-                    print('{} not in list!'.format(variable))
-                    self.set_ready(False)
+    def nfo_merge(self, data):
+        return [[data[i]] + nfo for i,nfo in enumerate(self.gridnfo)]
 
     def execute(self):
-
+        U = self.IO.load_from('data', 'U')
+        U = self.nfo_merge(U)
+        print(len(U))
+        #stuff
 
 
 if __name__ == '__main__':
 
-    path = '/home1/kd031/projects/icon/experiments/'
-    experiment_name = 'BCWcold'
-    cg = coarse_grain(path, experiment_name)
+    path = '/home1/kd031/projects/icon/experiments/BCWcold'
+    gridfile = r'iconR\dB\d{2}-grid_refined_\d{1}.nc'
+    datafile = r'BCWcold_R2B07_slice.nc'
+    cg = CoarseGrain(path, gridfile, datafile)
     print cg._ready
+    cg.execute()
 
 
